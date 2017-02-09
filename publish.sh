cfa2e623aca94d798727344e29d73f1922f91356
shahbazshueb <shahbazshueb@gmail.com>
2017-02-09 16:41:27 +0500

    publish script

--- publish.sh
+++ publish.sh
@@ -3,13 +3,13 @@
 
 if [[ `git status --porcelain` ]]; then
   echo "clean working directory"
-
+  exit 0
 fi
-versionText=$(cat build.sbt | grep "version := " | tr -d :)
+versionText=$(cat build.sbt | grep "version := ")
 
 sbt publish
 git checkout mvn-repo
 git add mvn-repo
-git commit -m $versionText
+git commit -m "$versionText"
 git push origin mvn-repo
 git checkout master
