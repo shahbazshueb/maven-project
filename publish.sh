1099c80ff59bcfe3eff076e01e5816f76b8d87c3
shahbazshueb <shahbazshueb@gmail.com>
2017-02-09 16:31:55 +0500

    removed pom.xml and publish.sh updated

--- publish.sh
+++ publish.sh
@@ -3,12 +3,13 @@
 
 if [[ `git status --porcelain` ]]; then
   echo "clean working directory"
-  exit 0
+
 fi
+versionText=$(cat build.sbt | grep "version := ")
 
 sbt publish
 git checkout mvn-repo
 git add mvn-repo
-git commit -m "commit message"
+git commit -m $versionText
 git push origin mvn-repo
 git checkout master
