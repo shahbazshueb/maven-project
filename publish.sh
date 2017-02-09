1b6fc2ad591f770517e52c7e46c7dd202233c659
shahbazshueb <shahbazshueb@gmail.com>
2017-02-09 18:11:52 +0500

    added functionality to check master branch

--- publish.sh
+++ publish.sh
@@ -1,19 +1,24 @@
 
 #!/bin/bash
 
+currentVersion=$(cat build.sbt | grep "version := ")
+
+if ! [[ `git branch | grep "\* master$"` ]]; then
+  echo "new artifact can only be published from master branch"
+  exit 0
+fi
+
 if [[ `git status --porcelain` ]]; then
   echo "clean working directory"
   exit 0
 fi
 
-versionText=$(cat build.sbt | grep "version := ")
-
-if [ -z "$versionText" ]; then
+if [ -z "$currentVersion" ]; then
   echo "version not properly mentioned in build.sbt"
   exit 0
 fi
 
-if [[ `git log --all --grep="$versionText" origin/mvn-repo` ]]; then
+if [[ `git log --all --grep="$currentVersion" origin/mvn-repo` ]]; then
   echo "version already published on github mvn-repo branch"
   exit 0
 fi
@@ -24,6 +29,6 @@ git checkout mvn-repo
 git reset --hard
 git pull origin mvn-repo
 git add mvn-repo
-git commit -m "$versionText"
+git commit -m "$currentVersion"
 git push origin mvn-repo
 git checkout master
