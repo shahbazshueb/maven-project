de336890e82122f465cbf26e4e45e8e39051d18e
shahbazshueb <shahbazshueb@gmail.com>
2017-02-15 17:56:45 +0500

    publish script updated

--- publish.sh
+++ publish.sh
@@ -1,29 +1,16 @@
 
 #!/bin/bash
 
-currentVersion=$(cat build.sbt | grep "version := " | egrep -o '([0-9]|\.)+')
+currentVersion=$(cat version.sbt | grep "version := " | egrep -o '([0-9]|\.)+')
 
-if ! [[ `git branch | grep "\* master$"` ]]; then
-  echo "new artifact can only be published from master branch"
-  exit 1
+
+if ["$1" == "-final"]; then
+    sbt -Dfinal release
+else
+    sbt release
+    currentVersion=$currentVersion"-SNAPSHOT"
 fi
 
-if [[ `git status --porcelain` ]]; then
-  echo "clean working directory required to publish artifact"
-  exit 1
-fi
-
-if [ -z "$currentVersion" ]; then
-  echo "version not properly mentioned in build.sbt"
-  exit 1
-fi
-
-if [[ `git fetch && git log --all --grep="version $currentVersion" origin/mvn-repo` ]]; then
-  echo "version already published on github mvn-repo branch"
-  exit 1
-fi
-
-sbt publish &&
 git checkout mvn-repo &&
 git reset --hard &&
 git pull origin mvn-repo &&
