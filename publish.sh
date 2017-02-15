954be23f24b049102a2e2c2e72c1cd35b42504d9
shahbazshueb <shahbazshueb@gmail.com>
2017-02-15 21:03:23 +0500

    script updated

--- publish.sh
+++ publish.sh
@@ -16,7 +16,7 @@ git reset --hard &&
 git pull origin mvn-repo &&
 rsync -av release/* mvn-repo/
 git add mvn-repo/ &&
-git commit -m "v$currentVersion" &&
+git commit -m "v"$currentVersion &&
 git push origin mvn-repo &&
 git checkout master &&
 sbt clean
